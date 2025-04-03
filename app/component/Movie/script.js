60fa1cf590470a8ca62153d307b556f532c96d72
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2025-04-03 16:20:39 +0200

    please

---
+++ app/component/Movie/script.js
@@ -0,0 +1,18 @@
+let templateFile = await fetch("./component/Movie/template.html");
+let template = await templateFile.text();
+
+let Movie = {};
+
+Movie.format = function (movies) {
+  let html = "";
+  movies.forEach(movie => {
+    let movieHtml = template;
+    movieHtml = movieHtml.replace("{{nom}}", movie.name);
+    movieHtml = movieHtml.replace("{{image}}", movie.image);
+
+    html += movieHtml
+  });
+  return html;
+};
+
+export { Movie };
