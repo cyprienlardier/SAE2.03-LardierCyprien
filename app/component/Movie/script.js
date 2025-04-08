f520b72efdbc38caa8dcde338c2c458fdaef07c5
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2025-04-09 00:24:02 +0200

    Itération 4 sans css

--- app/component/Movie/script.js
+++ app/component/Movie/script.js
@@ -1,18 +1,25 @@
 let templateFile = await fetch("./component/Movie/template.html");
 let template = await templateFile.text();
 
+
 let Movie = {};
 
-Movie.format = function (movies) {
-  let html = "";
-  movies.forEach((movie) => {
-    let movieHtml = template;
-    movieHtml = movieHtml.replace("{{titre}}", movie.name);
-    movieHtml = movieHtml.replace("{{image}}", movie.image);
-    movieHtml = movieHtml.replace("{{handler}}",`C.handlerDetail(${movie.id})`);
-    html += movieHtml;
-  });
+Movie.format = function(movie) {
+  let html = template;
+  html = html.replace("{{title}}", movie.name);
+  html = html.replace("{{imgs}}", movie.image);
+  html = html.replace(
+    "{{handler}}",
+    `C.handlerInfo(${movie.id})`);
   return html;
 };
+
+Movie.formatMany = function(movies) {
+    let html = "";
+    for (const r of movies) {
+      html = html + Movie.format(r);
+    }
+    return html;
+  };
 
 export { Movie };
