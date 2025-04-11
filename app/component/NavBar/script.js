36d24f815faca508c00cd9901a52bd50eb0484c7
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2025-04-11 12:41:42 +0200

    Ité 6

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -2,12 +2,13 @@ let templateFile = await fetch("./component/NavBar/template.html");
 let template = await templateFile.text();
 
 let NavBar = {};
-NavBar.format = function (handlerMovies, handlerProfil, profils) {
+NavBar.format = function (handlerAccueil, handlerProfil, profils) {
   let html = template;
-  html = html.replace("{{hAccueil}}", hAccueil);
+ 
+  html = html.replace("{{handlerAccueil}}", handlerAccueil);
   html = html.replace("{{handler}}", handlerProfil);
 
-  let profil = "";
+  let profil = `<option value="">Choisir un profil</option>`;	
   for (let i = 0; i < profils.length; i++) {
     let p = profils[i];
     profil += `<option value="${p.nom}" data-img="${p.avatar}" data-dob="${p.age}">${p.nom}</option>`;
