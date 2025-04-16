8830eb9622dea07540dd3eca78e4b6b1ad00fd22
cyprienlardier <cyprien.lardier@etu.unilim.fr>
2025-04-16 23:11:39 +0200

    ité 9&10 modification

--- SAE203/app/component/Movie/script.js
+++ SAE203/app/component/Movie/script.js
@@ -24,7 +24,7 @@ Movie.format = function (movie, isFavorisPage = false) {
     html = html.replace("{{buttonClass}}", "addFavoris_button");
     html = html.replace("{{buttonAction}}", `C.addFavoris(${movie.id})`);
     html = html.replace("{{buttonLabel}}", "Ajouter aux favoris");
-    html = html.replace("{{iconPath}}", "M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"); // Icône d'étoile
+    html = html.replace("{{iconPath}}", "M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"); 
   }
 
   return html;
